1a7fb87e677864a349d78547d646a3dbcdc1ba17
Alexandra Palocz <apalocz@princeton.edu>
2021-12-07 06:46:28 -0800

    responsive height for song iframe

--- src/pages/TardigradeSongPage.jsx
+++ src/pages/TardigradeSongPage.jsx
@@ -1,6 +1,6 @@
 const embedIframe = (
 <div 
-    style={{height: "533.33px", width: "300.00px", position: "relative"}} className="centered">
+    style={{height: "min(calc(100vh - 250px), 30rem)", width: "calc(min(calc(100vh - 250px), 30rem) * 300/533.33)", position: "relative"}} className="centered">
         <iframe allow="autoplay; gyroscope;" allowfullscreen height="100%" referrerpolicy="strict-origin" src="https://www.kapwing.com/e/6080d28e785c43009a5936d6" 
         style={{maxHeight: "100%",border: "0", height: "100%", left: "0", overflow: "hidden", position: "absolute", top: "0", width: "100%"}} title="Embedded content made on Kapwing" width="100%">
      </iframe></div>)
@@ -12,7 +12,6 @@ function TardigradeSongPage() {
   
       <div className="centered">
       <p><i>A short song in response to the classic icebreaker question, "What kind of animal (or plant, or fungus, ect.) would you be"? </i></p>
-      <br/>
       {embedIframe}
 
       </div>
