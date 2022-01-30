f13d70050387a565ea5b08bb294888769098b0c4
Alexandra Palocz <apalocz@princeton.edu>
2022-01-29 21:00:31 -0800

    Add In the Pines to Writing page

--- src/pages/WritingPage.jsx
+++ src/pages/WritingPage.jsx
@@ -1,5 +1,6 @@
 
 import eigRaft from "../images/eig-raft.jpg"
+import inThePines from "../images/in_the_pines_photo.jpg"
 import inSideHouse from "../images/In_side house.jpg"
 import wordJar from "../images/patchwork_poems/circle-word-jar.jpg"
 
@@ -10,6 +11,13 @@ const echoesLinkInfo = {
     altText: "Marker drawing depicting a girl floating down a river on a makeshift raft.",
     link: "/echoes_in_glass",
     title:"An interactive story cycle"
+}
+
+const inThePinesLinkInfo = {
+    imgSource: inThePines,
+    altText: "Photograph of a friendship bracelet on the ground, surrounded by pine needles",
+    link: "https://open.spotify.com/episode/73K0AQrR1OxuAjOoeL2evW?si=65f3e7274a4d44fa",
+    title:"A short play"
 }
 
 const inSideLinkInfo = {
@@ -31,15 +39,20 @@ function WritingPage() {
         <>
         <h2> Writing </h2>
 
-        <ImageLinkListElement linkImageInfo={echoesLinkInfo} title="Echoes in Glass" >
+        <ImageLinkListElement linkImageInfo={inThePinesLinkInfo} title="In the Pines" outsideLink>
+           <i>You go in together. You come out alone. </i> A short play originally selected for Theatre Intime's Student {" "}
+           Playwrights Festival in 2020, produced as an audio play instead due to the pandemic.
+        </ImageLinkListElement>
+
+        <ImageLinkListElement linkImageInfo={inSideLinkInfo} title="In/Side" outsideLink imageRight >
+            A comic about quarantine in a haunted house, involving tea, puzzles, and a love of bird motifs.
+        </ImageLinkListElement>
+
+        <ImageLinkListElement linkImageInfo={echoesLinkInfo} title="Echoes in Glass">
             An interactive theatrical cycle of stories, poems, and songs that follows the tale of two sisters, a bargain, and a bag of salt.
         </ImageLinkListElement>
 
-        <ImageLinkListElement linkImageInfo={inSideLinkInfo} title="In/Side" imageRight outsideLink >
-            A short comic about quarantine in a haunted house, involving tea, puzzles, and a love of bird motifs.
-        </ImageLinkListElement>
-
-        <ImageLinkListElement linkImageInfo={patchworkPoemsLinkInfo} title="Patchwork Poems" >
+        <ImageLinkListElement linkImageInfo={patchworkPoemsLinkInfo} title="Patchwork Poems" imageRight>
             A series of poems created by repurposing cut-out lines from a collection of love poetry.
         </ImageLinkListElement>
         
