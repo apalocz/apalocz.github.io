6c50e662ead746898c5edf344e0b92d9f9d8f676
Alexandra Palocz <apalocz@princeton.edu>
2022-01-30 02:57:50 -0800

    fix tardigrade page

--- src/pages/project_pages/TardigradeSongPage.jsx
+++ src/pages/project_pages/TardigradeSongPage.jsx
@@ -1,7 +1,7 @@
 const embedIframe = (
 <div 
     style={{height: "min(calc(100vh - 250px), 30rem)", width: "calc(min(calc(100vh - 250px), 30rem) * 300/533.33)", position: "relative"}} className="centered">
-        <iframe allow="autoplay; gyroscope;" allowfullscreen height="100%" referrerpolicy="strict-origin" src="https://www.kapwing.com/e/6080d28e785c43009a5936d6" 
+        <iframe allow="autoplay; gyroscope;" allowFullScreen height="100%" referrerPolicy="strict-origin" src="https://www.kapwing.com/e/6080d28e785c43009a5936d6" 
         style={{maxHeight: "100%",border: "0", height: "100%", left: "0", overflow: "hidden", position: "absolute", top: "0", width: "100%"}} title="Embedded content made on Kapwing" width="100%">
      </iframe></div>)
 
